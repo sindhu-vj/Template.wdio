d7c82c0488979095e63467bcac5bf2cc232cc375
Sindhu vijayaraman <sindhu.vijayaraman5841@stu.mtec.edu>
2025-03-20 14:13:36 -0600

    np

--- test/specs/test.e2e.js
+++ test/specs/test.e2e.js
@@ -2,11 +2,22 @@ import { expect } from '@wdio/globals'
 import LoginPage from '../pageobjects/login.page.js'
 import HomePage from '../pageobjects/home.js'
 
-describe('My Login application', () => {
+describe('Positive Test', () => {
     it('should login with valid credentials', async () => {
         await LoginPage.open()
         await LoginPage.login('standard_user', 'secret_sauce')
+
+        await LoginPage.open()
+        await LoginPage.login('problem_user', 'secret_sauce')
+
+        await LoginPage.open()
+        await LoginPage.login('performance_glitch_user', 'secret_sauce')
+
+        await LoginPage.open()
+        await LoginPage.login('error_user', 'secret_sauce')
+
+        await LoginPage.open()
+        await LoginPage.login('visual_user', 'secret_sauce')
       
     })
 })
-
